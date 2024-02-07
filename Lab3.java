e298bec092ac33727aed2113a26e0ba59e7e4cc5
gsarwar1 <156928580+gsarwar1@users.noreply.github.com>
2024-02-07 10:15:52 -0500

    Update Lab3.java

--- Lab3.java
+++ Lab3.java
@@ -10,18 +10,33 @@
 
 import java.util.Scanner;
 
-class Lab3 {
+public class BankAtm {
     public static void main(String[] args) {
-       System.out.println("This Program will allow you to use an atm")
-
-
-
-        if (choice.equals("w"))
-           System.out.println("How much would you like to withdraw");
-            double withraw = input.nextInt();
-            double newbalance = initialbalance - withdraw
+        System.out.println("This program will allow a customer who wants to deposit to, withdraw from, and check the balance of their bank account..");
+        Scanner scanner = new Scanner(System.in);
+        double initialbalance = 212.90;
+       
+        System.out.println("Enter the name");
+        String Name = scanner.nextLine();
+        System.out.println("Welcome " + Name + " What would you like to do, select d(deposit), w(withdraw), c(check balance), l(leave).");
+        String choice = scanner.nextLine();
+        while (!choice.equals("l")) {
+            if (choice.equals("d")) {
+            System.out.println("How much would you like to deposit");
+            double deposit = scanner.nextInt();
+            double newbalance = initialbalance + deposit; 
+            System.out.println("You now have $" + newbalance + " left in your account");
+            System.out.println("What would you like to do, select d(deposit), w(withdraw), c(check balance), l(leave).");
+            String choice2 = scanner.next();
+           } else if (choice.equals("w")) {
+            System.out.println("How much would you like to withdraw");
+            double withdraw = scanner.nextInt();
+            double newbalance = initialbalance - withdraw;
             System.out.println("You now have $" + newbalance + " left in your account");
 
 
+	    }
     }
 }
+}
+
