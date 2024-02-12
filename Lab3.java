5eb7d99cd735f3138e3173ab90ce1e91f4d87c69
brodib18 <156928886+brodib18@users.noreply.github.com>
2024-02-11 23:34:31 -0500

    Update Lab3.java

--- Lab3.java
+++ Lab3.java
@@ -1,42 +1,50 @@
 // Programmers: Gabe.S and Brodie.R
 // Course:  CS 212
 // Due Date:2/11/24
-// Lab Assignment: 
-// Problem Statement:
-// Data In:
-// Data Out:
+// Lab Assignment: 3
+// Problem Statement: You work at a bank and your current project is to design and implement a new interface for a new generation of ATMs.
+// Data In: choice of action, how much money to deal with
+// Data Out:money in bank acount
 // Credits: [Is your code based on an example in the book, in class, or something else?
 //            Reminder: you should never take code from the Internet or another person
-
 import java.util.Scanner;
 
-public class BankAtm {
+public class Main {
     public static void main(String[] args) {
-        System.out.println("This program will allow a customer who wants to deposit to, withdraw from, and check the balance of their bank account..");
-        Scanner scanner = new Scanner(System.in);
-        double initialbalance = 212.90;
-       
-        System.out.println("Enter the name");
-        String Name = scanner.nextLine();
-        System.out.println("Welcome " + Name + " What would you like to do, select d(deposit), w(withdraw), c(check balance), l(leave).");
-        String choice = scanner.nextLine();
+        Scanner input = new Scanner(System.in);
+        double initial_balance = 212.90;
+        double current_balance = initial_balance;
+
+        System.out.println("Enter your name");
+        String name = input.next();
+        System.out.println("Hello " + name + ". Would you like to ... d(deposit), w(withdraw), c(check balance), l(leave).");
+        String choice = input.next();
+
         while (!choice.equals("l")) {
             if (choice.equals("d")) {
-            System.out.println("How much would you like to deposit");
-            double deposit = scanner.nextInt();
-            double newbalance = initialbalance + deposit; 
-            System.out.println("You now have $" + newbalance + " left in your account");
-            System.out.println("What would you like to do, select d(deposit), w(withdraw), c(check balance), l(leave).");
-            String choice2 = scanner.next();
-           } else if (choice.equals("w")) {
-            System.out.println("How much would you like to withdraw");
-            double withdraw = scanner.nextInt();
-            double newbalance = initialbalance - withdraw;
-            System.out.println("You now have $" + newbalance + " left in your account");
+                System.out.println("How much would you like to deposit?");
+                double deposit = input.nextInt();
+                current_balance = current_balance + deposit;
+                System.out.println("You now have $" + current_balance + " left in your account");
+            } else if (choice.equals("w")) {
+                System.out.println("How much would you like to withdraw?");
+                double withdraw = input.nextInt();
+                if (withdraw <= current_balance) {
+                    current_balance = current_balance - withdraw;
+                    System.out.println("You now have $" + current_balance + " left in your account");
+                } else {
+                    System.out.println("You do not have enough money to withdraw that.");
+                }
+            } else if (choice.equals("c")) {
+                System.out.println("Your current balance is $" + current_balance);
+            } else {
+                System.out.println("Invalid choice. Please select d, w, c, or l.");
+            }
 
-
-	    }
+            System.out.println("What would you like to do? Select d(deposit), w(withdraw), c(check balance), l(leave).");
+            choice = input.next();
+        }
+        System.out.println("Thank you for banking with us!");
     }
-}
 }
 
